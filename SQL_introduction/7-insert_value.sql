e918685579388d5e9315a3827bd548c492c94a7d
DphenomenalALU <i.salami@alustudent.com>
2024-11-16 16:20:22 +0200

    Update 7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- A script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
--- Query to list all rows of a table
-SELECT * FROM first_table;
+-- A script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+-- Query to insert a new row
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
