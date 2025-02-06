2fe3f70f9c9d9c841548c963553da2f4ff7219ac
DphenomenalALU <i.salami@alustudent.com>
2025-02-06 10:58:51 +0000

    all

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,11 +1,3 @@
-#!/usr/bin/node
+const Rectangle = require('./0-main');
 
-const Rectangle = require('./4-rectangle');
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-}
-
-module.exports = Square;
+class
